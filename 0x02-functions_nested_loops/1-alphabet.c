64f1504e14a649e5fd8f14ea36c7bb1524fdc6ac
Thato Mongwe <thatomongwe47@gmail.com>
2023-02-21 17:15:10 +0000

    prints the alphabet, in lowercase, followed by a newline

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,19 @@
 #include "main.h"
 
-int main(void){
-    char letter;
-    letter = 'a';
-while(letter <= 'z'){
-    _putchar(letter);
-    letter++;
-}
-_putchar('\n');
+/**
+ * print_alphabet - prints the alphabet, in lowercase, followed by a newline
+ * Return: Always 0 (success)
+ */
+void print_alphabet(void)
+{
+	char ch;
+
+	ch = 'a';
+
+	while (ch <= 'z')
+	{
+		_putchar(ch);
+		ch++;
+	}
+	_putchar('\n');
 }
