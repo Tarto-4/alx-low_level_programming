5b38f2ee5c0ffc4f6d40181a08fd6990ff92fe88
Thato Mongwe <thatomongwe47@gmail.com>
2023-02-27 16:39:00 +0200

    Update 1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -2,14 +2,14 @@
 
 /**
  * swap_int - swaps the values of two integers
- * @a: pointer to first value
- * @b: pointer to second value
+ * @a: pointer 1
+ * @b: pointer 2
+ * Return: void
  */
+
 void swap_int(int *a, int *b)
 {
-	int c;
-
-	c = *a;
+	int x = *a;
 	*a = *b;
-	*b = c;
+	*b = x;
 }
