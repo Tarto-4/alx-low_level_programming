d2c1f05f0cca1673b7e623b1d0c2b3308b4dfc89
Thato Mongwe <thatomongwe47@gmail.com>
2023-03-10 23:46:23 +0000

    error correction

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,20 +1,19 @@
 #include "main.h"
-
 /**
- * _puts - prints string
+ * _puts - print a string
  * @str: string
- * Return: void
  */
 
 void _puts(char *str)
 {
-	int i = 0;
+	int i;
+
+	i = 0;
 
 	while (str[i] != '\0')
 	{
 		_putchar(str[i]);
 		i++;
 	}
-
 	_putchar('\n');
 }
