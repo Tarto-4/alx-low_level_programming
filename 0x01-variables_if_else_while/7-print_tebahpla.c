515d057157b3e54e034bdc761a47b32a81d361ae
Thato Mongwe <thatomongwe47@gmail.com>
2023-07-14 14:14:09 -0700

    Print out the English aplhabet in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * Print out the English alphabet in reverse
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'z'; c >= 'a'; c--)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
