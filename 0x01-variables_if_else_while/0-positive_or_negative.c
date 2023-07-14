3243059bcff71d822a64f220b95a3b7e91d0be6e
Thato Mongwe <thatomongwe47@gmail.com>
2023-07-14 04:49:30 -0700

    A program that determines whether a number is positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -8,12 +8,10 @@
  * A program that prints whether a number is positive or negative
  * Return: Always 0 (Success)
  */
-int main(voi)
+int main(void)
 {
 	int n;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
 	if (n > 0){
 		printf("%d is positive\n", n);
 	} else if (n < 0){
