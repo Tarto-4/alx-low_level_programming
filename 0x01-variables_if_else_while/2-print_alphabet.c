2f039a9bfb34c2f552e0c695a943950f5ecd03c9
Thato Mongwe <thatomongwe47@gmail.com>
2023-07-14 11:19:57 -0700

    A prgram that prints out the English alphabet in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * A program that prints out the English Alphabet in lowercase
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char a;
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		putchar(a);
+		putchar("\n");
+	}
+	return (0);
+}
