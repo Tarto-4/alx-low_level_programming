74c89c22580409864ca926590970efd3c2e87f84
Thato Mongwe <thatomongwe47@gmail.com>
2023-07-18 10:01:20 -0700

    A function that prints the sign of the number

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -14,11 +14,11 @@ int print_sign(int c)
 	{
 		_putchar('+');
 		return (1);
-	} else if (c == 0)
+	} else if (n == 0)
 	{
 		_putchar('0');
 		return (0);
-	} else
+	} else if (n < 0)
 	{
 		_putchar('-');
 		return (-1);
