91271cbf1c11046e43ba9fef24e3d9055f5ba449
Thato Mongwe <thatomongwe47@gmail.com>
2023-07-18 05:55:18 -0700

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -9,11 +9,12 @@ void print_alphabet(void);
 
 void print_alphabet(void)
 {
-	char c;
+	char c[];
+	int i;
 
-	for (c = '97'; c < '127'; c++)
+	for (i = '97'; i < '127'; i++)
 	{
-		_putchar(c);
+		_putchar(c[i]);
 	}
 	_putchar('\n');
 	return (0);
