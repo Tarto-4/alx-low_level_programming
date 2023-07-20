4515173d990c3b107a13553f104553811c2c1dc2
Thato Mongwe <thatomongwe47@gmail.com>
2023-07-20 02:23:58 -0700

    Add: 0-main.c, main.h

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - Test a function that prints whether a number
+ * is positive or negative
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+	int n;
+
+	n = 0;
+	positive_or_negative(n);
+
+	return (0);
+}
