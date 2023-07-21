c88501d1bd140ee36dc5e60028aec79efbe664e2
Thato Mongwe <thatomongwe47@gmail.com>
2023-07-21 00:39:03 -0700

    print most numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,21 +1,19 @@
 #include "main.h"
-
 /**
  * print_most_numbers - print prints 0-9 excluding 2 & 4
- * @n: integer to be printed out
  * Return: 0 on success
  */
 void print_most_numbers(void)
 {
-	int n = 48;
+	int i = 48;
 
-	while (n <= 57)
+	while (i <= 57)
 	{
-		if (n != 50 && n != 52)
+		if (i != 50 && i != 52)
 		{
-			_putchar(n);
+			_putchar(i);
 		}
-		n++;
+		i++;
 	}
 	_putchar('\n');
 }
