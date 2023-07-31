efe90d08c5d0a259dc696d9b184d25810f938b2b
Thato Mongwe <thatomongwe47@gmail.com>
2023-07-31 00:05:59 -0700

    rset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - function that reassigns a value of a variable to 98.
+ * @n: value to reassign
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
