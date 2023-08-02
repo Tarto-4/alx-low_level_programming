4a2bbac079ad3f74624f245b8dfc6a05e36ba6e0
Thato Mongwe <thatomongwe47@gmail.com>
2023-08-02 05:24:06 -0700

    add: main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -4,10 +4,6 @@
 
 #include <stdio.h>
 
-void reset_to_98(int *n);
-
-#endif /* MAIN_H */
-=======
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
@@ -20,4 +16,5 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
->>>>>>> f70d8b2ebd0eff10fe50c6f607df29fbcd2fcb5e
+
+#endif /* MAIN_H */
