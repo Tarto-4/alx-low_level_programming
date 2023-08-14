587b6d62bc5d5110d9899b1cd0aaa07cf7804408
Thato Mongwe <thatomongwe47@gmail.com>
2023-08-14 06:34:48 -0700

    A macro-function that computes the absolute value of a number

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,4 +1,4 @@
-#ifndef 3-FUNCTION_LIKE_MACRO_H
-#define 3-FUNCTION_LIKE_MACRO_H
-#define ABS(x) ((x < 0) ? (-x) : (x))
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+#define ABS(x) ((x < 0) ? -(x) : (x))
 #endif
