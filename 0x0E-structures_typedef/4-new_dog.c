180f119ddf3fc4dfcd4065778a28ac006f34cdc2
Thato Mongwe <thatomongwe47@gmail.com>
2023-08-15 05:30:49 -0700

    create a new dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,58 @@
+#include "main.h"
+
+/**
+ * _strdup - returns a pointer to a newly allocated space in memory
+ * @str: string
+ * Return: pointer to a newly allocated space in memory
+ */
+char _strdup(char *str)
+{
+	int length, i;
+	char *arr;
+
+	if (str == NULL)
+	{
+		return (NULL):
+	}
+	length = 0;
+	while (*(str + length) != '\0')
+	{
+		length++;
+	}
+	arr = (char*) malloc(length * sizeof(char) + 1);
+	if (arr == NULL)
+		return (NULL);
+	for (i = 0; i < length; i++)
+	{
+		arr[i] = str[i];
+	}
+	arr[i] = '\0';
+	return (arr);
+}
+/**
+ * new_dog - creates a new dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ * Return: new_dog (dog_t)
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *doggy;
+	char *doggy_name, *doggy_owner;
+
+	doggy = malloc(sizeof(dog_t));
+	if (doggy == NULL)
+		return (NULL);
+	doggy_owner = _strdup(owner);
+	if (doggy_owner == NULL)
+	{
+		free(doggy_name);
+		free(doggy);
+		return (NULL);
+	}
+	doggy->name = doggy_name;
+	doggy->age = age;
+	doggy->owner = doggy_owner;
+	return (doggy);
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -20,4 +20,5 @@ typedef struct dog
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 typedef my_dog dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
 #endif /* DOG_H */
