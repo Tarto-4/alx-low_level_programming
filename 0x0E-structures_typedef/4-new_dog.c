2d34c5e988888f842f654d4602c0a0c985ed64ca
Thato Mongwe <thatomongwe47@gmail.com>
2023-08-15 05:58:51 -0700

    create a new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -21,7 +21,7 @@ char *_strdup(char *str)
 		length++;
 	}
 
-	arr = (char*) malloc(length * sizeof(char) + 1);
+	arr = (char *) malloc(length * sizeof(char) + 1);
 	if (arr == NULL)
 		return (NULL);
 	for (i = 0; i < length; i++)
